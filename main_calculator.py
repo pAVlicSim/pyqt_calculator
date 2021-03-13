a74bfac345757b90b1216091f0b021e05cf7887d
Pavel Simakov <simakoff.pasha@yandex.ru>
2021-03-13 16:25:38 +0300

    добавлены возведение в степень и извлечение  корня. Переработан интерфейс.доделан my_style.gss

--- main_calculator.py
+++ main_calculator.py
@@ -1,9 +1,41 @@
 from _decimal import setcontext
-from decimal import Decimal, DivisionByZero, InvalidOperation, Context, getcontext
-
-from PyQt5 import QtWidgets
-
+from decimal import Decimal, DivisionByZero, InvalidOperation, Context
+from PyQt5 import QtWidgets, QtCore
 import my_form_calculator
+
+
+def calculation_degree(number: str, number_list: list):
+    index_split = []
+    for j in number_list:
+        if j != -1:
+            index_split.append(number.rfind(j))
+    normal_number = number[:max(index_split) + 1]
+    small_number = number[max(index_split) + 1:]
+    for i in small_number:
+        if i == '⁰':
+            small_number = small_number.replace('⁰', '0')
+        elif i == '¹':
+            small_number = small_number.replace('¹', '1')
+        elif i == '²':
+            small_number = small_number.replace('²', '2')
+        elif i == '³':
+            small_number = small_number.replace('³', '3')
+        elif i == '⁴':
+            small_number = small_number.replace('⁴', '4')
+        elif i == '⁵':
+            small_number = small_number.replace('⁵', '5')
+        elif i == '⁶':
+            small_number = small_number.replace('⁶', '6')
+        elif i == '⁷':
+            small_number = small_number.replace('⁷', '7')
+        elif i == '⁸':
+            small_number = small_number.replace('⁸', '8')
+        elif i == '⁹':
+            small_number = small_number.replace('⁹', '9')
+        print(normal_number, small_number)
+    result = Decimal(normal_number.replace(',', '.')) ** Decimal(small_number.replace(',', '.'))
+    print(result)
+    return result
 
 
 def calculation_multiplication_division(action: str, number_1: str, number_2: str):
@@ -24,6 +56,35 @@ def calculation_addition_subtraction(action: str, number_1: str, number_2: str):
     return result
 
 
+def calculation_root(numberRoot: str):
+    numberRootList = numberRoot.split('√')
+    print(numberRootList)
+    for i in numberRootList[0]:
+        if i == '⁰':
+            numberRootList[0] = numberRootList[0].replace('⁰', '0')
+        elif i == '¹':
+            numberRootList[0] = numberRootList[0].replace('¹', '1')
+        elif i == '²':
+            numberRootList[0] = numberRootList[0].replace('²', '2')
+        elif i == '³':
+            numberRootList[0] = numberRootList[0].replace('³', '3')
+        elif i == '⁴':
+            numberRootList[0] = numberRootList[0].replace('⁴', '4')
+        elif i == '⁵':
+            numberRootList[0] = numberRootList[0].replace('⁵', '5')
+        elif i == '⁶':
+            numberRootList[0] = numberRootList[0].replace('⁶', '6')
+        elif i == '⁷':
+            numberRootList[0] = numberRootList[0].replace('⁷', '7')
+        elif i == '⁸':
+            numberRootList[0] = numberRootList[0].replace('⁸', '8')
+        elif i == '⁹':
+            numberRootList[0] = numberRootList[0].replace('⁹', '9')
+    print(numberRootList)
+    result = Decimal(numberRootList[1].replace(',', '.')) * (1 / Decimal(numberRootList[0]))
+    return result
+
+
 class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):
     def __init__(self, parent=None):
         QtWidgets.QFrame.__init__(self, parent)
@@ -32,9 +93,11 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):
         self.result = ''
         self.edit_1 = []
         self.numberButton = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',']
-        self.actionButton = [' + ', ' - ', ' × ', ' ÷ ']
+        self.actionButton = [' + ', ' - ', ' × ', ' ÷ ', '√']
+        self.small_numberButton = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
         self.full_edit = []
         self.full_calculation_list = []
+        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)
 
         self.my_context = Context(prec=35, flags=[], traps=[InvalidOperation, DivisionByZero])
         setcontext(self.my_context)
@@ -52,19 +115,33 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):
         self.pushButton_comma.clicked.connect(lambda: self.continuous_input(','))
         self.pushButton_plus_minus.clicked.connect(lambda: self.continuous_input('±'))
 
-        self.pushButton_root.clicked.connect(lambda: self.continuous_input(' √ '))
+        self.pushButtonTop0.clicked.connect(lambda: self.continuous_input('⁰'))
+        self.pushButtonTop1.clicked.connect(lambda: self.continuous_input('¹'))
+        self.pushButtonTop2.clicked.connect(lambda: self.continuous_input('²'))
+        self.pushButtonTop3.clicked.connect(lambda: self.continuous_input('³'))
+        self.pushButtonTop4.clicked.connect(lambda: self.continuous_input('⁴'))
+        self.pushButtonTop5.clicked.connect(lambda: self.continuous_input('⁵'))
+        self.pushButtonTop6.clicked.connect(lambda: self.continuous_input('⁶'))
+        self.pushButtonTop7.clicked.connect(lambda: self.continuous_input('⁷'))
+        self.pushButtonTop8.clicked.connect(lambda: self.continuous_input('⁸'))
+        self.pushButtonTop9.clicked.connect(lambda: self.continuous_input('⁹'))
+
+        self.pushButton_root.clicked.connect(lambda: self.continuous_input('√'))
         self.pushButton_plus.clicked.connect(lambda: self.continuous_input(' + '))
         self.pushButton_minus.clicked.connect(lambda: self.continuous_input(' - '))
         self.pushButton_divide.clicked.connect(lambda: self.continuous_input(' ÷ '))
         self.pushButton_multiply.clicked.connect(lambda: self.continuous_input(' × '))
         self.pushButton_degree.clicked.connect(lambda: self.continuous_input(' xⁿ '))
 
-        self.pushButton_result.clicked.connect(self.processing_list_multiplication_division)
+        self.pushButton_result.clicked.connect(self.processing_list_root)
         self.pushButton_clear_all.clicked.connect(self.clear_edit_all)
         self.pushButton_clear_one.clicked.connect(self.lineEdit_clear)
 
+        self.pushButton_cursor_left.clicked.connect(self.cursor_left)
+        self.pushButton_cursor_right.clicked.connect(self.cursor_right)
+
     def continuous_input(self, symbol):
-        if symbol in self.actionButton or symbol in self.numberButton:
+        if symbol in self.actionButton or symbol in self.numberButton or self.small_numberButton:
             self.lineEdit_1.insert(symbol)
             self.full_edit = str(self.lineEdit_1.text()).split()
             print(self.full_edit)
@@ -72,12 +149,34 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):
             self.lineEdit_1.insert('-')
             self.full_edit = str(self.lineEdit_1.text()).split()
             print(self.full_edit)
-        elif self.lineEdit_1.isModified():
-            self.full_edit = str(self.lineEdit_1.text()).split()
-            print(self.full_edit)
+
+    def processing_list_root(self):
+        self.lineEdit_1.end(False)
+        try:
+            while range(len(self.full_edit)):
+                for i in range(len(self.full_edit)):
+                    if '√' in self.full_edit[i]:
+                        self.full_edit[i] = str(calculation_root(self.full_edit[i])).replace('.', ',')
+                break
+            print('после преобразования root', self.full_edit)
+            self.processing_list_degree()
+        except InvalidOperation:
+            pass
+
+    def processing_list_degree(self):
+        try:
+            while range(len(self.full_edit)):
+                for i in range(len(self.full_edit)):
+                    if self.full_edit[i][0] in self.numberButton and self.full_edit[i][-1] in self.small_numberButton:
+                        self.full_edit[i] = (str(calculation_degree(self.full_edit[i], self.numberButton)).
+                                             replace('.', ','))
+                break
+            print('после преобразования degree', self.full_edit)
+            self.processing_list_multiplication_division()
+        except InvalidOperation:
+            pass
 
     def processing_list_multiplication_division(self):
-        self.lineEdit_1.setCursorPosition(len(self.lineEdit_1.text()))
         try:
             while '×' in self.full_edit or '÷' in self.full_edit:
                 for i in range(len(self.full_edit)):
@@ -111,7 +210,7 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):
                                                              self.full_edit[i + 1])).replace('.', ',')
                         self.full_edit.pop(i - 1)
                         self.full_edit.pop(i)
-                        print('после processing_list:', self.full_edit)
+                        print('после processing_list addition:', self.full_edit)
                         break
         except (IndexError, InvalidOperation):
             QtWidgets.QMessageBox.information(window, "Ошибка ввода.", "Вы ощиблись при вводе!\nБудьте внимательнее!",
@@ -128,6 +227,16 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):
             self.lineEdit_1.clear()
         else:
             pass
+
+    def cursor_left(self):
+        self.lineEdit_1.cursorBackward(False, 1)
+        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)
+        print(self.lineEdit_1.cursorPosition())
+
+    def cursor_right(self):
+        self.lineEdit_1.cursorForward(False, 1)
+        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)
+        print(self.lineEdit_1.cursorPosition())
 
     def lineEdit_clear(self):
         self.full_edit.clear()
