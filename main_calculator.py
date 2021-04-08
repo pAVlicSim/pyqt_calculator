43d20f70a189a6fc76820b2692b163f82c703526
PavelSimakov <simaroff.pasha@yandex.ru>
2021-04-08 21:00:45 +0300

    реализован метод processing_list_bracket(self) для расчета вычислений со скобками

--- main_calculator.py
+++ main_calculator.py
@@ -34,9 +34,9 @@ def calculation_degree(number: str, number_list: list):
             small_number = small_number.replace('⁸', '8')
         elif i == '⁹':
             small_number = small_number.replace('⁹', '9')
-        print(normal_number, small_number)
+        print('calculation_degree()-1', normal_number, small_number)
     result = Decimal(normal_number.replace(',', '.')) ** Decimal(small_number.replace(',', '.'))
-    print(result)
+    print('calculation_degree()-2', result)
     return result
 
 
@@ -96,7 +96,7 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
         self.actionButton = [' + ', ' - ', ' × ', ' ÷ ', '√', '( ', ' )']  # лист математических символов
         self.small_numberButton = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']  # лист надстрочных цифр
         self.full_edit = []  # лист вводимых символов в lineEdit1
-        self.full_calculation_list = []  # ещё не используется
+        self.sub_full_edit = []  # ещё не используется
         self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  # настройка фокуса, от нажатия кнопок на окне программы
 
         self.my_context = Context(prec=35, flags=[], traps=[InvalidOperation, DivisionByZero])  # контекст для Decimal
@@ -158,57 +158,77 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
     def processing_list_bracket(self):
         a = '('
         b = ')'
-        for i in range(len(self.full_edit)):
-            if self.full_edit[i] == a and self.full_edit[i + 1] != a:
-                print(self.full_edit[i: self.full_edit.index(b) + 1])
+        if a in self.full_edit or b in self.full_edit and self.full_edit.count(a) == self.full_edit.count(b):
+            while a in self.full_edit or b in self.full_edit:
+                for i in range(len(self.full_edit)):
+                    if (a not in self.full_edit[i + 1: self.full_edit.index(b)] and
+                            b not in self.full_edit[i + 1: self.full_edit.index(b)]):
+                        self.sub_full_edit = self.full_edit[i: self.full_edit.index(b) + 1]
+                        print('1', self.sub_full_edit)
+                        del self.full_edit[i: self.full_edit.index(b)]
+                        self.sub_full_edit.remove(a)
+                        self.sub_full_edit.remove(b)
+                        print('2', self.sub_full_edit)
+                        self.processing_list_root(self.sub_full_edit)
+                        self.full_edit[i] = self.sub_full_edit[0]
+                        print('3', self.sub_full_edit)
+                        print(self.full_edit)
+                        break
+            self.processing_list_root(self.full_edit)
+        else:
+            self.processing_list_root(self.full_edit)
 
-    def processing_list_root(self):  # функция ищет объекты со знаком "корня" и отправляет их в метод для расчёта
+    # функция ищет объекты со знаком "корня" и отправляет их в метод для расчёта
+    def processing_list_root(self, calc_list: list):
         self.lineEdit_1.end(False)  # перемещает курсор в конец строки
         try:  # перехват исключении
-            while range(len(self.full_edit)):  # цикл будет исполнятся пока не дойдёт до конца списка
-                for i in range(len(self.full_edit)):  # цикл перебирает full_edit по индексам
-                    if '√' in self.full_edit[i]:  # если индекс содержит знак "корня", то
+            while range(len(calc_list)):  # цикл будет исполнятся пока не дойдёт до конца списка
+                for i in range(len(calc_list)):  # цикл перебирает full_edit по индексам
+                    if '√' in calc_list[i]:  # если индекс содержит знак "корня", то
                         # меняем значение индекса на полученное в соответствующем методе
-                        self.full_edit[i] = str(calculation_root(self.full_edit[i])).replace('.', ',')
+                        calc_list[i] = str(calculation_root(calc_list[i])).replace('.', ',')
                 break  # возвращаемся во в while
-            print('после преобразования root', self.full_edit)
+            print('после преобразования root', calc_list)
         except (InvalidOperation, IndexError):  # если возникает исключение показываем диалоговое окно
             QtWidgets.QMessageBox.information(window, "Ошибка ввода.", "Вы ошиблись при вводе!\nБудьте внимательнее!",
                                               buttons=QtWidgets.QMessageBox.Ok,
                                               defaultButton=QtWidgets.QMessageBox.Ok)
         else:  # если всё проходит хорошо идём дальше
-            self.processing_list_degree()
+            self.processing_list_degree(calc_list)
 
-    def processing_list_degree(self):  # функция ищет объекты с возведением в степень
+    def processing_list_degree(self, calc_list: list):  # функция ищет объекты с возведением в степень
         try:  # перехват исключений
-            while range(len(self.full_edit)):  # цикл исполняется пока не дойдёт до конца списка
-                for i in range(len(self.full_edit)):  # цикл перебирает full_edit по индексам
+            while range(len(calc_list)):  # цикл исполняется пока не дойдёт до конца списка
+                for i in range(len(calc_list)):  # цикл перебирает full_edit по индексам
                     # если в начале строки символ из numbersButton, а в конце символ из small_numbersButton
-                    if self.full_edit[i][0] in self.numberButton and self.full_edit[i][-1] in self.small_numberButton:
+                    if calc_list[i][0] in self.numberButton and calc_list[i][-1] in self.small_numberButton:
                         # меняем значение индекса на полученное в соответствующем методе
-                        self.full_edit[i] = (str(calculation_degree(self.full_edit[i], self.numberButton)).
-                                             replace('.', ','))
+                        print('1', calc_list[i])
+                        calc_list[i] = str(calculation_degree(calc_list[i], self.numberButton)).replace('.', ',')
+                        print('2', calc_list[i])
                 break  # возвращаемся во в while
-            print('после преобразования degree', self.full_edit)
+            print('после преобразования degree', calc_list)
         except (InvalidOperation, IndexError):  # если возникает исключение показываем диалоговое окно
             QtWidgets.QMessageBox.information(window, "Ошибка ввода.", "Вы ошиблись при вводе!\nБудьте внимательнее!",
                                               buttons=QtWidgets.QMessageBox.Ok,
                                               defaultButton=QtWidgets.QMessageBox.Ok)
         else:  # если всё проходит хорошо идём дальше
-            self.processing_list_multiplication_division()
+            self.processing_list_multiplication_division(calc_list)
 
-    def processing_list_multiplication_division(self):  # эта функция проверяет есть ли в примере деление и умножение
+    # эта функция проверяет есть ли в примере деление и умножение
+    def processing_list_multiplication_division(self, calc_list: list):
         try:  # перехват исключений
-            while '×' in self.full_edit or '÷' in self.full_edit:  # цикл работает пока в full_edit есть '×' и '÷'
-                for i in range(len(self.full_edit)):  # цикл перебирает full_edit по индексам
-                    if self.full_edit[i] == '×' or self.full_edit[i] == '÷':  # если находит '×' или '÷'
+            # цикл работает пока в full_edit есть '×' и '÷'
+            while '×' in calc_list or '÷' in calc_list:
+                for i in range(len(calc_list)):  # цикл перебирает full_edit по индексам
+                    # если находит '×' или '÷'
+                    if calc_list[i] == '×' or calc_list[i] == '÷':
                         # меняет значение этого индекса на вычисляемое в методе  calculation_multiplication_division
-                        self.full_edit[i] = str(
-                            calculation_multiplication_division(self.full_edit[i], self.full_edit[i - 1],
-                                                                self.full_edit[i + 1])).replace('.', ',')
-                        self.full_edit.pop(i - 1)  # удаляем индекс до знака действия
-                        self.full_edit.pop(i)  # удаляем индекс после знака действия
-                        print('после processing_list:', self.full_edit)
+                        calc_list[i] = str(calculation_multiplication_division(calc_list[i], calc_list[i - 1],
+                                                                               calc_list[i + 1])).replace('.', ',')
+                        calc_list.pop(i - 1)  # удаляем индекс до знака действия
+                        calc_list.pop(i)  # удаляем индекс после знака действия
+                        print('после processing_list:', calc_list)
                         break  # возвращаемся в начало цикла
         except DivisionByZero:  # если захочется делить на ноль
             # показываем окно об ошибке
@@ -221,27 +241,30 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                                               buttons=QtWidgets.QMessageBox.Ok,
                                               defaultButton=QtWidgets.QMessageBox.Ok)
         else:  # если всё проходит хорошо идём дальше
-            self.processing_list_addition_subtraction()
+            self.processing_list_addition_subtraction(calc_list)
 
-    def processing_list_addition_subtraction(self):  # ну вот добрались до сложения и вычитания
+    def processing_list_addition_subtraction(self, calc_list: list):  # ну вот добрались до сложения и вычитания
         try:  # перехват исключений
-            while '+' in self.full_edit or '-' in self.full_edit:  # функция работает аналогично предыдущей
-                for i in range(len(self.full_edit)):  #
-                    if self.full_edit[i] == '+' or self.full_edit[i] == '-':  #
+            # функция работает аналогично предыдущей
+            while '+' in calc_list or '-' in calc_list:
+                for i in range(len(calc_list)):  #
+                    if calc_list[i] == '+' or calc_list[i] == '-':  #
                         #
-                        self.full_edit[i] = str(
-                            calculation_addition_subtraction(self.full_edit[i], self.full_edit[i - 1],
-                                                             self.full_edit[i + 1])).replace('.', ',')
-                        self.full_edit.pop(i - 1)  #
-                        self.full_edit.pop(i)  #
-                        print('после processing_list addition:', self.full_edit)
+                        calc_list[i] = str(
+                            calculation_addition_subtraction(calc_list[i],
+                                                             calc_list[i - 1],
+                                                             calc_list[i + 1])).replace('.', ',')
+                        calc_list.pop(i - 1)  #
+                        calc_list.pop(i)  #
+                        print('после processing_list addition:', calc_list)
                         break  #
         except (IndexError, InvalidOperation):  #
             QtWidgets.QMessageBox.information(window, "Ошибка ввода.", "Вы ошиблись при вводе!\nБудьте внимательнее!",
                                               buttons=QtWidgets.QMessageBox.Ok,
                                               defaultButton=QtWidgets.QMessageBox.Ok)
         else:  # если всё хорошо
-            self.calculation_result()
+            if (len(self.full_edit) == 1) and ('(' not in self.full_edit):
+                self.calculation_result()
 
     def calculation_result(self):
         result = str(self.full_edit[0])
@@ -266,16 +289,16 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
 
     #
     def lineEdit_clear(self):
-        self.full_edit.clear()
+        self.calc_list.clear()
         self.lineEdit_1.backspace()
-        self.full_edit = str(self.lineEdit_1.text()).split()
-        print(self.full_edit)
+        self.calc_list = str(self.lineEdit_1.text()).split()
+        print(self.calc_list)
 
     #
     def clear_edit_all(self):
         self.textEdit_result.clear()
         self.lineEdit_1.clear()
-        self.full_edit.clear()
+        self.calc_list.clear()
 
 
 if __name__ == "__main__":
