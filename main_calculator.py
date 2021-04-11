ea8089a7778c424635bf3b3e9bf375e7ed8b074e
PavelSimakov <simaroff.pasha@yandex.ru>
2021-04-11 11:33:53 +0300

    добавлен QComboBox

--- main_calculator.py
+++ main_calculator.py
@@ -1,7 +1,8 @@
 from _decimal import setcontext
 from decimal import Decimal, DivisionByZero, InvalidOperation, Context
+from typing import List, Any, Union
 
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 
 import my_form_calculator
 
@@ -40,21 +41,21 @@ def calculation_degree(number: str, number_list: list):
     return result
 
 
-def calculation_multiplication_division(action: str, number_1: str, number_2: str):
+def calculation_multiplication_division(sub_full_edit: list):
     result = 0
-    if action == '×':
-        result = Decimal(number_1.replace(',', '.')) * Decimal(number_2.replace(',', '.'))
-    elif action == '÷':
-        result = Decimal(number_1.replace(',', '.')) / Decimal(number_2.replace(',', '.'))
+    if sub_full_edit[1] == '×':
+        result = Decimal(sub_full_edit[0].replace(',', '.')) * Decimal(sub_full_edit[2].replace(',', '.'))
+    elif sub_full_edit[1] == '÷':
+        result = Decimal(sub_full_edit[0].replace(',', '.')) / Decimal(sub_full_edit[2].replace(',', '.'))
     return result
 
 
-def calculation_addition_subtraction(action: str, number_1: str, number_2: str):
+def calculation_addition_subtraction(sub_full_edit: list):
     result = 0
-    if action == '+':
-        result = Decimal(number_1.replace(',', '.')) + Decimal(number_2.replace(',', '.'))
-    elif action == '-':
-        result = Decimal(number_1.replace(',', '.')) - Decimal(number_2.replace(',', '.'))
+    if sub_full_edit[1] == '+':
+        result = Decimal(sub_full_edit[0].replace(',', '.')) + Decimal(sub_full_edit[2].replace(',', '.'))
+    elif sub_full_edit[1] == '-':
+        result = Decimal(sub_full_edit[0].replace(',', '.')) - Decimal(sub_full_edit[2].replace(',', '.'))
     return result
 
 
@@ -88,6 +89,8 @@ def calculation_root(numberRoot: str):
 
 
 class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный класс
+    prec_list: list[Union[str, Any]]
+
     def __init__(self, parent=None):
         QtWidgets.QFrame.__init__(self, parent)
         self.setupUi(self)
@@ -96,10 +99,13 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
         self.actionButton = [' + ', ' - ', ' × ', ' ÷ ', '√', '( ', ' )']  # лист математических символов
         self.small_numberButton = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']  # лист надстрочных цифр
         self.full_edit = []  # лист вводимых символов в lineEdit1
-        self.sub_full_edit = []  # ещё не используется
         self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  # настройка фокуса, от нажатия кнопок на окне программы
+        self.prec_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '15', '20']  # кортеж для comboBox
 
-        self.my_context = Context(prec=35, flags=[], traps=[InvalidOperation, DivisionByZero])  # контекст для Decimal
+        self.comboBox.addItems(self.prec_list)
+        self.comboBox.setCurrentText('3')
+
+        self.my_context = Context(prec=3, flags=[], traps=[InvalidOperation, DivisionByZero])  # контекст для Decimal
         setcontext(self.my_context)  # подключение контекста
 
         # кнопки вводят символы через функцию continuous_input
@@ -155,28 +161,29 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
             self.full_edit = str(self.lineEdit_1.text()).split()  # преобразование строки в лист по пробелам
             print(self.full_edit)  #
 
+    # преобразует full_edit убирая скобки
     def processing_list_bracket(self):
-        a = '('
-        b = ')'
-        if a in self.full_edit or b in self.full_edit and self.full_edit.count(a) == self.full_edit.count(b):
-            while a in self.full_edit or b in self.full_edit:
-                for i in range(len(self.full_edit)):
-                    if (a not in self.full_edit[i + 1: self.full_edit.index(b)] and
-                            b not in self.full_edit[i + 1: self.full_edit.index(b)]):
-                        self.sub_full_edit = self.full_edit[i: self.full_edit.index(b) + 1]
-                        print('1', self.sub_full_edit)
-                        del self.full_edit[i: self.full_edit.index(b)]
-                        self.sub_full_edit.remove(a)
-                        self.sub_full_edit.remove(b)
-                        print('2', self.sub_full_edit)
-                        self.processing_list_root(self.sub_full_edit)
-                        self.full_edit[i] = self.sub_full_edit[0]
-                        print('3', self.sub_full_edit)
-                        print(self.full_edit)
-                        break
-            self.processing_list_root(self.full_edit)
-        else:
-            self.processing_list_root(self.full_edit)
+        left_bracket = '('  # левая скобка
+        right_bracket = ')'  # правая скобка
+        # если в full_edit есть скобки
+        if (left_bracket in self.full_edit or right_bracket in self.full_edit and
+                self.full_edit.count(left_bracket) == self.full_edit.count(right_bracket)):
+            while left_bracket in self.full_edit or right_bracket in self.full_edit:  # цикл работает пока есть скобки
+                for i in range(len(self.full_edit)):  # цикл обходит основной список по индексу
+                    # если в срезе,между левой и правой скобок,нет скобок
+                    if (left_bracket not in self.full_edit[i + 1: self.full_edit.index(right_bracket)] and
+                            right_bracket not in self.full_edit[i + 1: self.full_edit.index(right_bracket)]):
+                        # создаём новый список из среза
+                        sub_full_edit = self.full_edit[i: self.full_edit.index(right_bracket) + 1]
+                        del self.full_edit[i: self.full_edit.index(right_bracket)]  # удаляем срез из  основного списка
+                        sub_full_edit.remove(left_bracket)  # удаляем из нового списка скобки
+                        sub_full_edit.remove(right_bracket)  #
+                        self.processing_list_root(sub_full_edit)  # считаем результат в новом списке
+                        self.full_edit[i] = sub_full_edit[0]  # вставляем результат в основной список
+                        break  # возвращаемся в начало цикла for искать следующую пару скобок
+            self.processing_list_root(self.full_edit)  # когда все скобки убраны, запускаем расчёт основного списка
+        else:  # если изначально в основном списке нет скобок
+            self.processing_list_root(self.full_edit)  # запускаем расчёт основного списка
 
     # функция ищет объекты со знаком "корня" и отправляет их в метод для расчёта
     def processing_list_root(self, calc_list: list):
@@ -196,16 +203,15 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
         else:  # если всё проходит хорошо идём дальше
             self.processing_list_degree(calc_list)
 
-    def processing_list_degree(self, calc_list: list):  # функция ищет объекты с возведением в степень
+    # функция ищет объекты с возведением в степень
+    def processing_list_degree(self, calc_list: list):
         try:  # перехват исключений
             while range(len(calc_list)):  # цикл исполняется пока не дойдёт до конца списка
                 for i in range(len(calc_list)):  # цикл перебирает full_edit по индексам
                     # если в начале строки символ из numbersButton, а в конце символ из small_numbersButton
                     if calc_list[i][0] in self.numberButton and calc_list[i][-1] in self.small_numberButton:
                         # меняем значение индекса на полученное в соответствующем методе
-                        print('1', calc_list[i])
                         calc_list[i] = str(calculation_degree(calc_list[i], self.numberButton)).replace('.', ',')
-                        print('2', calc_list[i])
                 break  # возвращаемся во в while
             print('после преобразования degree', calc_list)
         except (InvalidOperation, IndexError):  # если возникает исключение показываем диалоговое окно
@@ -224,10 +230,10 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                     # если находит '×' или '÷'
                     if calc_list[i] == '×' or calc_list[i] == '÷':
                         # меняет значение этого индекса на вычисляемое в методе  calculation_multiplication_division
-                        calc_list[i] = str(calculation_multiplication_division(calc_list[i], calc_list[i - 1],
-                                                                               calc_list[i + 1])).replace('.', ',')
-                        calc_list.pop(i - 1)  # удаляем индекс до знака действия
-                        calc_list.pop(i)  # удаляем индекс после знака действия
+                        sub_full_edit = calc_list[i - 1: i + 2]
+                        calc_list[i] = str(calculation_multiplication_division(sub_full_edit)).replace('.', ',')
+                        del calc_list[i + 1]
+                        del calc_list[i - 1]
                         print('после processing_list:', calc_list)
                         break  # возвращаемся в начало цикла
         except DivisionByZero:  # если захочется делить на ноль
@@ -243,19 +249,18 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
         else:  # если всё проходит хорошо идём дальше
             self.processing_list_addition_subtraction(calc_list)
 
-    def processing_list_addition_subtraction(self, calc_list: list):  # ну вот добрались до сложения и вычитания
+    # ну вот добрались до сложения и вычитания
+    def processing_list_addition_subtraction(self, calc_list: list):
         try:  # перехват исключений
             # функция работает аналогично предыдущей
             while '+' in calc_list or '-' in calc_list:
                 for i in range(len(calc_list)):  #
                     if calc_list[i] == '+' or calc_list[i] == '-':  #
-                        #
-                        calc_list[i] = str(
-                            calculation_addition_subtraction(calc_list[i],
-                                                             calc_list[i - 1],
-                                                             calc_list[i + 1])).replace('.', ',')
-                        calc_list.pop(i - 1)  #
-                        calc_list.pop(i)  #
+                        sub_full_edit = calc_list[i - 1: i + 2]
+                        print('1', sub_full_edit)
+                        calc_list[i] = str(calculation_addition_subtraction(sub_full_edit)).replace('.', ',')
+                        del calc_list[i + 1]
+                        del calc_list[i - 1]
                         print('после processing_list addition:', calc_list)
                         break  #
         except (IndexError, InvalidOperation):  #
@@ -263,50 +268,50 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                                               buttons=QtWidgets.QMessageBox.Ok,
                                               defaultButton=QtWidgets.QMessageBox.Ok)
         else:  # если всё хорошо
-            if (len(self.full_edit) == 1) and ('(' not in self.full_edit):
-                self.calculation_result()
+            if (len(self.full_edit) == 1) and ('(' not in self.full_edit):  #
+                self.calculation_result()  #
 
-    def calculation_result(self):
-        result = str(self.full_edit[0])
-        if result != "None":
-            self.lineEdit_1.insert(' = ' + str(result).replace('.', ','))
-            self.textEdit_result.append(self.lineEdit_1.text())
-            self.lineEdit_1.clear()
-        else:
+    def calculation_result(self):  #
+        result = str(self.full_edit[0])  #
+        if result != "None":  #
+            self.lineEdit_1.insert(' = ' + str(result).replace('.', ','))  #
+            self.textEdit_result.append(self.lineEdit_1.text())  #
+            self.lineEdit_1.clear()  #
+        else:  #
             pass
 
     #
     def cursor_left(self):
-        self.lineEdit_1.cursorBackward(False, 1)
-        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)
-        print(self.lineEdit_1.cursorPosition())
+        self.lineEdit_1.cursorBackward(False, 1)  #
+        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  #
+        print(self.lineEdit_1.cursorPosition())  #
 
     #
     def cursor_right(self):
-        self.lineEdit_1.cursorForward(False, 1)
-        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)
-        print(self.lineEdit_1.cursorPosition())
+        self.lineEdit_1.cursorForward(False, 1)  #
+        self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  #
+        print(self.lineEdit_1.cursorPosition())  #
 
     #
     def lineEdit_clear(self):
-        self.calc_list.clear()
-        self.lineEdit_1.backspace()
-        self.calc_list = str(self.lineEdit_1.text()).split()
-        print(self.calc_list)
+        self.calc_list.clear()  #
+        self.lineEdit_1.backspace()  #
+        self.calc_list = str(self.lineEdit_1.text()).split()  #
+        print(self.calc_list)  #
 
     #
-    def clear_edit_all(self):
-        self.textEdit_result.clear()
-        self.lineEdit_1.clear()
-        self.calc_list.clear()
+    def clear_edit_all(self):  #
+        self.textEdit_result.clear()  #
+        self.lineEdit_1.clear()  #
+        self.full_edit.clear()  #
 
 
-if __name__ == "__main__":
-    import sys
+if __name__ == "__main__":  #
+    import sys  #
 
-    app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)  #
     window = MyWindow()  # Создаем экземпляр класса
-    window.setWindowTitle("Калькулятор")  #
-    window.setStyleSheet(open('my_style.qss', 'r').read())  #
+    window.setWindowTitle("Калькулятор")  # название программы
+    window.setStyleSheet(open('my_style.qss', 'r').read())  # подключение QSS
     window.show()  # Отображаем окно
     sys.exit(app.exec())  # Запускаем цикл обработки событий
---
+++ test_main_calculator.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_calculation_degree(self):
+        self.fail()
+
+    def test_calculation_multiplication_division(self):
+        self.fail()
+
+    def test_calculation_addition_subtraction(self):
+        self.fail()
+
+    def test_calculation_root(self):
+        self.fail()
+
+    def test_my_window(self):
+        self.fail()
+
+
+class TestMyWindow(TestCase):
+    def test_continuous_input(self):
+        self.fail()
+
+    def test_processing_list_bracket(self):
+        self.fail()
+
+    def test_processing_list_root(self):
+        self.fail()
+
+    def test_processing_list_degree(self):
+        self.fail()
+
+    def test_processing_list_multiplication_division(self):
+        self.fail()
+
+    def test_processing_list_addition_subtraction(self):
+        self.fail()
+
+    def test_calculation_result(self):
+        self.fail()
+
+    def test_cursor_left(self):
+        self.fail()
+
+    def test_cursor_right(self):
+        self.fail()
+
+    def test_line_edit_clear(self):
+        self.fail()
+
+    def test_clear_edit_all(self):
+        self.fail()
