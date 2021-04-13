8df05fd586eede3a7b5d9050d3d9494aeddad4e6
PavelSimakov <simaroff.pasha@yandex.ru>
2021-04-13 21:23:36 +0300

    Добавлен QComboBox для регулировки точночти вычислений. Доделан Qss.

--- main_calculator.py
+++ main_calculator.py
@@ -1,47 +1,29 @@
 from _decimal import setcontext
 from decimal import Decimal, DivisionByZero, InvalidOperation, Context
-from typing import List, Any, Union
 
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QDesktopWidget
 
 import my_form_calculator
 
 
-def calculation_degree(number: str, number_list: list):
+def calculation_degree(number: str, small_number_list: list[str], number_list: list[str]):
     index_split = []
-    for j in number_list:
-        if j != -1:
-            index_split.append(number.rfind(j))
+    number_dict = dict(zip(small_number_list, number_list))
+    for split in number_list:
+        if split != -1:
+            index_split.append(number.rfind(split))
     normal_number = number[:max(index_split) + 1]
     small_number = number[max(index_split) + 1:]
     for i in small_number:
-        if i == '⁰':
-            small_number = small_number.replace('⁰', '0')
-        elif i == '¹':
-            small_number = small_number.replace('¹', '1')
-        elif i == '²':
-            small_number = small_number.replace('²', '2')
-        elif i == '³':
-            small_number = small_number.replace('³', '3')
-        elif i == '⁴':
-            small_number = small_number.replace('⁴', '4')
-        elif i == '⁵':
-            small_number = small_number.replace('⁵', '5')
-        elif i == '⁶':
-            small_number = small_number.replace('⁶', '6')
-        elif i == '⁷':
-            small_number = small_number.replace('⁷', '7')
-        elif i == '⁸':
-            small_number = small_number.replace('⁸', '8')
-        elif i == '⁹':
-            small_number = small_number.replace('⁹', '9')
-        print('calculation_degree()-1', normal_number, small_number)
+        for j in number_dict:
+            if i == j:
+                small_number = small_number.replace(i, number_dict[j])
     result = Decimal(normal_number.replace(',', '.')) ** Decimal(small_number.replace(',', '.'))
-    print('calculation_degree()-2', result)
     return result
 
 
-def calculation_multiplication_division(sub_full_edit: list):
+def calculation_multiplication_division(sub_full_edit: list[str]):
     result = 0
     if sub_full_edit[1] == '×':
         result = Decimal(sub_full_edit[0].replace(',', '.')) * Decimal(sub_full_edit[2].replace(',', '.'))
@@ -50,7 +32,7 @@ def calculation_multiplication_division(sub_full_edit: list):
     return result
 
 
-def calculation_addition_subtraction(sub_full_edit: list):
+def calculation_addition_subtraction(sub_full_edit: list[str]):
     result = 0
     if sub_full_edit[1] == '+':
         result = Decimal(sub_full_edit[0].replace(',', '.')) + Decimal(sub_full_edit[2].replace(',', '.'))
@@ -59,53 +41,36 @@ def calculation_addition_subtraction(sub_full_edit: list):
     return result
 
 
-def calculation_root(numberRoot: str):
+def calculation_root(numberRoot: str, small_list: list[str], normal_list: list[str]):
     numberRootList = numberRoot.split('√')
-    print(numberRootList)
+    number_dict = dict(zip(small_list, normal_list))
     for i in numberRootList[0]:
-        if i == '⁰':
-            numberRootList[0] = numberRootList[0].replace('⁰', '0')
-        elif i == '¹':
-            numberRootList[0] = numberRootList[0].replace('¹', '1')
-        elif i == '²':
-            numberRootList[0] = numberRootList[0].replace('²', '2')
-        elif i == '³':
-            numberRootList[0] = numberRootList[0].replace('³', '3')
-        elif i == '⁴':
-            numberRootList[0] = numberRootList[0].replace('⁴', '4')
-        elif i == '⁵':
-            numberRootList[0] = numberRootList[0].replace('⁵', '5')
-        elif i == '⁶':
-            numberRootList[0] = numberRootList[0].replace('⁶', '6')
-        elif i == '⁷':
-            numberRootList[0] = numberRootList[0].replace('⁷', '7')
-        elif i == '⁸':
-            numberRootList[0] = numberRootList[0].replace('⁸', '8')
-        elif i == '⁹':
-            numberRootList[0] = numberRootList[0].replace('⁹', '9')
-    print(numberRootList)
+        for small_number in number_dict:
+            if i == small_number:
+                numberRootList[0] = numberRootList[0].replace(i, number_dict[small_number])
     result = Decimal(numberRootList[1].replace(',', '.')) ** (1 / Decimal(numberRootList[0]))
     return result
 
 
 class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный класс
-    prec_list: list[Union[str, Any]]
 
     def __init__(self, parent=None):
         QtWidgets.QFrame.__init__(self, parent)
         self.setupUi(self)
 
-        self.numberButton = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',']  # лист обычных цифр
+        self.number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',']  # лист обычных цифр
         self.actionButton = [' + ', ' - ', ' × ', ' ÷ ', '√', '( ', ' )']  # лист математических символов
-        self.small_numberButton = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']  # лист надстрочных цифр
+        self.small_number = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']  # лист надстрочных цифр
         self.full_edit = []  # лист вводимых символов в lineEdit1
         self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  # настройка фокуса, от нажатия кнопок на окне программы
-        self.prec_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '15', '20']  # кортеж для comboBox
+        self.prec_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '15', '20', '25']  # кортеж для comboBox
+        self.prec = '000000'
 
         self.comboBox.addItems(self.prec_list)
-        self.comboBox.setCurrentText('3')
+        self.comboBox.setCurrentText(str(len(self.prec)))
 
-        self.my_context = Context(prec=3, flags=[], traps=[InvalidOperation, DivisionByZero])  # контекст для Decimal
+        # контекст для Decimal
+        self.my_context = Context(prec=25, flags=[], traps=[InvalidOperation, DivisionByZero])
         setcontext(self.my_context)  # подключение контекста
 
         # кнопки вводят символы через функцию continuous_input
@@ -149,10 +114,21 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
         self.pushButton_cursor_left.clicked.connect(self.cursor_left)  # кнопка передвигает курсор влево
         self.pushButton_cursor_right.clicked.connect(self.cursor_right)  # кнопка передвигает курсор вправо
 
+        self.comboBox.activated[str].connect(self.tincture_of_prec)
+
+    def center(self):
+        frame_geometry = self.frameGeometry()
+        center_desktop = QDesktopWidget().availableGeometry().center()
+        frame_geometry.moveCenter(center_desktop)
+        self.move(frame_geometry.topLeft())
+
+    def tincture_of_prec(self):
+        self.prec = '0' * int(self.comboBox.currentIndex() + 1)
+
     # функция вводит символы в lineEdit1
     def continuous_input(self, symbol):  # символ передаётся при нажатии соответствующей кнопки
         # если символ входит в листы цифр и действий
-        if symbol in self.numberButton or symbol in self.small_numberButton or symbol in self.actionButton:
+        if symbol in self.number or symbol in self.small_number or symbol in self.actionButton:
             self.lineEdit_1.insert(symbol)  # символ вставляется в конец строки
             self.full_edit = str(self.lineEdit_1.text()).split()  # преобразование строки в лист по пробелам
             print(self.full_edit)  #
@@ -193,7 +169,7 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                 for i in range(len(calc_list)):  # цикл перебирает full_edit по индексам
                     if '√' in calc_list[i]:  # если индекс содержит знак "корня", то
                         # меняем значение индекса на полученное в соответствующем методе
-                        calc_list[i] = str(calculation_root(calc_list[i])).replace('.', ',')
+                        calc_list[i] = str(calculation_root(calc_list[i], self.small_number, self.number))
                 break  # возвращаемся во в while
             print('после преобразования root', calc_list)
         except (InvalidOperation, IndexError):  # если возникает исключение показываем диалоговое окно
@@ -209,9 +185,9 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
             while range(len(calc_list)):  # цикл исполняется пока не дойдёт до конца списка
                 for i in range(len(calc_list)):  # цикл перебирает full_edit по индексам
                     # если в начале строки символ из numbersButton, а в конце символ из small_numbersButton
-                    if calc_list[i][0] in self.numberButton and calc_list[i][-1] in self.small_numberButton:
+                    if calc_list[i][0] in self.number and calc_list[i][-1] in self.small_number:
                         # меняем значение индекса на полученное в соответствующем методе
-                        calc_list[i] = str(calculation_degree(calc_list[i], self.numberButton)).replace('.', ',')
+                        calc_list[i] = str(calculation_degree(calc_list[i], self.small_number, self.number))
                 break  # возвращаемся во в while
             print('после преобразования degree', calc_list)
         except (InvalidOperation, IndexError):  # если возникает исключение показываем диалоговое окно
@@ -231,7 +207,7 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                     if calc_list[i] == '×' or calc_list[i] == '÷':
                         # меняет значение этого индекса на вычисляемое в методе  calculation_multiplication_division
                         sub_full_edit = calc_list[i - 1: i + 2]
-                        calc_list[i] = str(calculation_multiplication_division(sub_full_edit)).replace('.', ',')
+                        calc_list[i] = str(calculation_multiplication_division(sub_full_edit))
                         del calc_list[i + 1]
                         del calc_list[i - 1]
                         print('после processing_list:', calc_list)
@@ -258,7 +234,7 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                     if calc_list[i] == '+' or calc_list[i] == '-':  #
                         sub_full_edit = calc_list[i - 1: i + 2]
                         print('1', sub_full_edit)
-                        calc_list[i] = str(calculation_addition_subtraction(sub_full_edit)).replace('.', ',')
+                        calc_list[i] = str(calculation_addition_subtraction(sub_full_edit))
                         del calc_list[i + 1]
                         del calc_list[i - 1]
                         print('после processing_list addition:', calc_list)
@@ -269,35 +245,36 @@ class MyWindow(QtWidgets.QFrame, my_form_calculator.Ui_Form):  # главный 
                                               defaultButton=QtWidgets.QMessageBox.Ok)
         else:  # если всё хорошо
             if (len(self.full_edit) == 1) and ('(' not in self.full_edit):  #
-                self.calculation_result()  #
+                self.output_result()  #
 
-    def calculation_result(self):  #
-        result = str(self.full_edit[0])  #
-        if result != "None":  #
-            self.lineEdit_1.insert(' = ' + str(result).replace('.', ','))  #
-            self.textEdit_result.append(self.lineEdit_1.text())  #
+    def output_result(self):  #
+        if '.' in self.full_edit[0]:
+            result = str(Decimal(self.full_edit[0]).quantize(Decimal('1.' + self.prec))).replace('.', ',')  #
+            if result != "None":  #
+                self.textEdit_exsmple.append(self.lineEdit_1.text())  #
+                self.textEdit_result.append(result)
+                self.lineEdit_1.clear()  #
+        else:
+            result = self.full_edit[0]
+            self.textEdit_exsmple.append(self.lineEdit_1.text())  #
+            self.textEdit_result.append(result)
             self.lineEdit_1.clear()  #
-        else:  #
-            pass
 
     #
     def cursor_left(self):
         self.lineEdit_1.cursorBackward(False, 1)  #
         self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  #
-        print(self.lineEdit_1.cursorPosition())  #
 
     #
     def cursor_right(self):
         self.lineEdit_1.cursorForward(False, 1)  #
         self.lineEdit_1.setFocus(QtCore.Qt.OtherFocusReason)  #
-        print(self.lineEdit_1.cursorPosition())  #
 
     #
     def lineEdit_clear(self):
-        self.calc_list.clear()  #
+        self.full_edit.clear()  #
         self.lineEdit_1.backspace()  #
         self.calc_list = str(self.lineEdit_1.text()).split()  #
-        print(self.calc_list)  #
 
     #
     def clear_edit_all(self):  #
